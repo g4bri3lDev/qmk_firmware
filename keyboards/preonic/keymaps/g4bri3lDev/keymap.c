df648549991fb7230ff4b901705232a7585b745e
gabriel <g.lackermeier@gmx.de>
2020-12-12 18:53:18 +0100

    moved brackets  and equals to more sensible position

--- keyboards/preonic/keymaps/g4bri3lDev/keymap.c
+++ keyboards/preonic/keymaps/g4bri3lDev/keymap.c
@@ -104,20 +104,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   ~  | LCLK |  MUP | RCLK | MWUP |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * |   ~  | LCLK |  MUP | RCLK | MWUP |   %  |   ^  |   &  |   *  |   [  |   ]  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |MLEFT | MDOWN|MRIGHT| MWCLK|      |      |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |MWDOWN|      |      |ISO ~ |ISO | |      |      |      |
+ * |      |      |      |      |MWDOWN|      |      |ISO ~ |ISO | |      |      | Mute |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-  KC_TILD, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
+  KC_TILD, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, KC_DEL,  \
   _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN3, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, _______, _______, _______, KC_WH_D, _______, _______,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______, \
+  _______, _______, _______, _______, KC_WH_D, _______, _______,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, KC_MUTE, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
@@ -129,17 +129,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   1  |   2  |   3  |   -  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   0  |ISO / |      |   +  | Mute |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   0  |ISO / |   _  |   +  |  =   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      |  {   |   [  |   ]  |   }  |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_P7,   KC_P8,   KC_P9,   KC_PSLS, KC_BSPC, \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_DEL,  \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_P0,   KC_NUBS, _______, KC_PPLS, KC_MUTE, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_P0,   KC_NUBS, KC_UNDS, KC_PPLS, KC_EQL, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR  \
 ),
 
 /* Adjust (Lower + Raise)
