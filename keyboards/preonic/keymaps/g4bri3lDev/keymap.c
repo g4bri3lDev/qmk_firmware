165f0022897133b6ec3fd581926e34494120afe5
gabriel <g.lackermeier@gmx.de>
2021-01-20 21:30:16 +0100

    * Added Insert in lower and raise * Added Macro Support * Added shortcuts for file and calc * Added pgup etc.

--- keyboards/preonic/keymaps/g4bri3lDev/keymap.c
+++ keyboards/preonic/keymaps/g4bri3lDev/keymap.c
@@ -104,20 +104,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   ~  | LCLK |  MUP | RCLK | MWUP |   %  |   ^  |   &  |   *  |   [  |   ]  | Del  |
+ * |   ~  | LCLK |  MUP | RCLK | MWUP |   %  |   ^  |   &  |   *  |   [  |   ]  |Insert|
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |MLEFT | MDOWN|MRIGHT| MWCLK|      |      |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |MWDOWN|      |      |ISO ~ |ISO | |      |      | Mute |
+ * |      |      |      |      |MWDOWN|      |      | PGUP | PGDN | HOME | END  | Mute |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-  KC_TILD, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, KC_DEL,  \
+  KC_TILD, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, KC_INS,  \
   _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN3, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, _______, _______, _______, KC_WH_D, _______, _______,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, KC_MUTE, \
+  _______, _______, _______, _______, KC_WH_D, _______, _______, KC_PGUP, KC_PGDN, KC_HOME, KC_END,  KC_MUTE, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   /  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   4  |   5  |   6  |   *  | Del  |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   4  |   5  |   6  |   *  |Insert|
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   1  |   2  |   3  |   -  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_P7,   KC_P8,   KC_P9,   KC_PSLS, KC_BSPC, \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_DEL,  \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_INS,  \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_BSLS, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_P0,   KC_NUBS, KC_UNDS, KC_PPLS, KC_EQL, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR  \
@@ -148,19 +148,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |  RGB | Reset|      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | CYCLE|      |      |Aud on|AudOff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|      |      |
+ * | CYCLE|      |      |Aud on|AudOff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|DMREC1|DMREC2|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      |      |      |      |
+ * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff| CALC | MYCM |      |DMPLY1|DMPLY2|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * |      |      |      |      |      |             |      |      |      |      |DMRSTP|
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_grid( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
   RGB_TOG, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL,  \
-  RGB_MOD, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______, \
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+  RGB_MOD, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  DM_REC1, DM_REC2, \
+  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  KC_CALC, KC_MYCM, _______, DM_PLY1, DM_PLY2, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, DM_RSTP  \
 )
 
 
--- keyboards/preonic/keymaps/g4bri3lDev/rules.mk
+++ keyboards/preonic/keymaps/g4bri3lDev/rules.mk
@@ -21,13 +21,14 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
 WS2812_DRIVER = bitbang
 API_SYSEX_ENABLE = no
+DYNAMIC_MACRO_ENABLE = yes
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 #SLEEP_LED_ENABLE = yes
 
 # SERIAL_LINK_ENABLE = yes
-ENCODER_ENABLE = yes
+ENCODER_ENABLE = no
 DIP_SWITCH_ENABLE = yes
 
 LAYOUTS = ortho_5x12
